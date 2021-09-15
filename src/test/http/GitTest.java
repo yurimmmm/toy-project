8b9fcb983c576ef1cdfdad2f087be5aed58d3419
yurimmmm <89139623+yurimmmm@users.noreply.github.com>
2021-09-15 20:25:39 +0900

    conflict 연습

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,10 +2,10 @@ package test.http;
 
 public class GitTest {
 
-	private static final String DESC = "local GIT Test용 클래스입니다.";
+	private static final String DESC = "[GITHUB]local GIT Test용 클래스입니다.";
 	private static final String DEV = "dev브랜치 테스트 시작";
-	private static final String DEV_FIN = "dev브랜치 테스트 종료";
+	private static final String DEV_FIN = "[GITHUB]dev브랜치 테스트 종료";
 	private static final String CONFLIC = "Conflic 테스트";
-	private static final String MERGETOOL = "mergetool 사용";
+	private static final String MERGETOOL = "[GITHUB]mergetool 사용";
 	
 }
